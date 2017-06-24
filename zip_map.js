27fd7dadc344b0c5ad82a8588ef00c57586602b8
Lucia Kocincova <lucia.koc@gmail.com>
2017-06-24 20:32:42 +0100

    Upgrade of d3: v3 → v4
    Updated the version of d3 to the latest one
    (currently v4.9.1)

--- zip_map.js
+++ zip_map.js
@@ -1,6 +1,6 @@
 /**
- * Visualization of Slovakian zip codes. 
- * 
+ * Visualization of Slovakian zip codes.
+ *
  * Data downloaded from Wikipedia (https://sk.wikipedia.org/wiki/Zoznam_slovensk%C3%BDch_obc%C3%AD_a_vojensk%C3%BDch_obvodov)
  * Visualization uses Mercator projection; zoom adapted from bl.ocks.org/mbostock/eec4a6cda2f573574a11
  *
@@ -16,13 +16,16 @@
 	/**
 	 * Function firstly parses the given file and then visualizes it.
 	 *
-	 * @param {string} file tab separated file 
+	 * @param {string} file tab separated file
 	 */
 	function data2Vis( file ) {
 
 		d3.tsv( file, function ( error, data ) {
 
-			if ( error ) return error;
+			if ( error ) {
+				console.log(error);
+				return error;
+			}
 
 			visualize( parseData( data ) );
 
@@ -43,7 +46,7 @@
 				 * @return {object} city object representing a city
 				 */
 				function createCity( cityInfo ) {
-					
+
 					var zip = cityInfo.zip.replace(/\s/g, '');
 					var population = parseInt( cityInfo.population.replace(/\s/g, '') );
 					var coordinates = /(.+)°N (.+)°E/g.exec( cityInfo.gps );
@@ -81,45 +84,38 @@
 				var medianLon = d3.median( data, function(d) { return d.lon; } );
 
 				// scales and functions
-				var radius = d3.scale.sqrt()
+				var radius = d3.scaleSqrt()
 					.domain( d3.extent( data, function(d) { return d.population; } ) )
 					.range( [2, 20] );
 
-				var cityColor = d3.scale.ordinal()
+				var cityColor = d3.scaleOrdinal()
 					.domain( d3.range( 10 ) )
 					.range( ['#2ca02c', '#bcbd22', '#ff7f0e', '#d62728', '#8c564b', '#7f7f7f', '#17becf', '#1f77b4', '#9467bd', '#e377c2'] );
 
-				var projection = d3.geo.mercator();
+				var projection = d3.geoMercator()
+					.translate( [width/2 - scaleInitial/3 - 50, height/2 + scaleInitial - 150] )
+					.scale( scaleInitial );
 
-				var zoom = d3.behavior.zoom()
-					.translate( [width/2, height/2] )
-					.scale( scaleInitial )
-					.scaleExtent( [scaleInitial, 15*scaleInitial] )
+				var zoom = d3.zoom()
+					.scaleExtent( [1, 15] )
 					.on('zoom', zoomed);
 
 				// add basic tooltip
-				d3.select(".vis")
+				d3.select('.vis')
 					.append('div')
 						.attr('class', 'city-tooltip')
 						.style('opacity', 0);
 
 				// draw elements
-				var svg = d3.select('.vis')
+				var vis = d3.select('.vis')
 					.append('svg')
 						.attr('width', width)
 						.attr('height', height)
 						.style('border', '1px solid black')
-					.append('g');
 
-				svg.append('rect')
-					.attr('class', 'overlay')
-					.attr('width', width)
-					.attr('height', height);
+				var svg = vis.append('g');
 
-				var circleGroup = svg.append('g');
-
-				svg.call( zoom )
-					.call( zoom.event );
+				vis.call( zoom );
 
 				// draw cities
 				update();
@@ -134,7 +130,7 @@
 
 					// color legend only when checked
 					d3.selectAll('.color-box')
-						.style('background-color', function(d, i) { 
+						.style('background-color', function(d, i) {
 							return document.getElementById('colorCheck').checked ? cityColor( i ) : nonMatchColor;
 						});
 				});
@@ -157,9 +153,19 @@
 					var colorCheck = document.getElementById('colorCheck').checked;
 					var input = document.getElementById('zipInput').value;
 
-					var cities = circleGroup.selectAll('circle')
+					var cities = svg.selectAll('circle')
 						.data( data );
-					
+
+					cities.exit()
+						.transition()
+						.duration( 700 )
+						.attr('r', 0)
+						.remove();
+
+					cities.transition()
+							.attr('r', function(d) { return populationCheck ? radius( d.population ) : 3; })
+							.attr('fill', function(d) { return color( input, d.zip, colorCheck ); });
+
 					cities.enter()
 						.append('circle')
 							.attr('class', 'city')
@@ -167,18 +173,9 @@
 							.attr('cy', function(d) { return projection( [d.lon, d.lat] )[1]; })
 							.on('mouseover', mouseover)
 							.on('mouseout', moseout)
-							.text( function(d) { return d.name; } );
-
-					cities.transition()
-							.duration( 800 )
+							.transition()
 							.attr('r', function(d) { return populationCheck ? radius( d.population ) : 3; })
 							.attr('fill', function(d) { return color( input, d.zip, colorCheck ); });
-
-					cities.exit()
-						.transition()
-						.duration( 700 )
-						.attr('r', 0)
-						.remove();
 				}
 
 				/**
@@ -216,7 +213,7 @@
 				 * Creates a tooltip over the circle representing a city
 				 * and creates a stroke around the circle to highlight it.
 				 *
-				 * @param {object} city 
+				 * @param {object} city
 				 */
 				function mouseover( city ){
 					var circle = d3.select( this )
@@ -227,25 +224,25 @@
 						.transition()
 						.style('opacity', 1);
 
-					var absMouseCoord = d3.mouse( d3.select( this )[0][0] );
+					var absMouseCoord = d3.mouse( this );
 
 					d3.select('.city-tooltip')
 						.html( showTooltip )
-						.style('left', absMouseCoord[0] + "px")
-						.style('top', absMouseCoord[1] + "px");
+						.style('left', absMouseCoord[0] + 'px')
+						.style('top', absMouseCoord[1] + 'px');
 
 					/**
 					 * Shows formatted tooltip with information about the city.
-					 * 
-					 * @return {string} 
+					 *
+					 * @return {string}
 					 */
 					function showTooltip() {
 						var tooltip = '<p>' + city.zip + ' - ' + city.name + '</p>';
-						
+
 						if ( document.getElementById('populationCheck').checked ) {
 							// create user friendly format for population numbers
 							var number = city.population.toString().split('').reverse().join('');
-							number = number.replace(/(\d{3})/g, "$1 ");
+							number = number.replace(/(\d{3})/g, '$1 ');
 							number = number.split('').reverse().join('');
 
 							tooltip += '<p>' + number + ' citizens </p>';
@@ -272,17 +269,15 @@
 				 * and renderes only those cities in the extent of the zoom scale.
 				 */
 				function zoomed() {
-					projection
-						.translate( zoom.translate() )
-						.scale( zoom.scale() )
-						.center( [medianLon, medianLat] );
+					// translate the whole group holding all circles
+					svg.attr('transform', d3.event.transform);
 
-					circleGroup.selectAll('circle')
-						.attr('cx', function(d) { return projection( [d.lon, d.lat] )[0]; })
-						.attr('cy', function(d) { return projection( [d.lon, d.lat] )[1]; });
+					// do not translate each circle individually
+					// svg.selectAll('circle')
+					// 	.attr('transform', d3.event.transform);
 				}
 			}
 		});
-	}	
+	}
 
 })( this, document );
